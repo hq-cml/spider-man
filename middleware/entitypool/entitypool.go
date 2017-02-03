8777c5dcc532f70ba01ae55e306a9b5bbec8b942
hq <hq_cml@163.com>
2017-02-03 17:36:46 +0800

    实体池接口

---
+++ middleware/entitypool/entitypool.go
@@ -0,0 +1,18 @@
+package entitypool
+
+/*
+ * 实体池：池操作的抽象
+ */
+
+//实体接口类型
+type Entity interface {
+    Id() uint32 // ID获取方法
+}
+
+//实体池的接口类型
+type PoolIntfs interface {
+    Get() (Entity, error) //获取
+    Put(e Entity) error   //归还
+    Total() uint32        //池子容量
+    Used() uint32         //池子中已使用的数量
+}
