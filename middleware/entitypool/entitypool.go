03024e4ef7b9a1d81e6e9b514a8b58f5476b8b04
hq <hq_cml@163.com>
2017-02-08 08:48:51 +0800

    中间件:实体池初始化

--- middleware/entitypool/entitypool.go
+++ middleware/entitypool/entitypool.go
@@ -1,5 +1,11 @@
 package entitypool
 
+import (
+    "reflect"
+    "fmt"
+    "errors"
+)
+
 /*
  * 实体池：池操作的抽象
  */
@@ -16,4 +22,62 @@ type PoolIntfs interface {
     Total() uint32        //池子容量
     Used() uint32         //池子中已使用的数量
 }
+
+//实体池类型，实现PoolIntfs接口
+type Pool struct {
+    total     uint32        //池容量
+    etype     reflect.Type  //池子中实体的类型
+    genEntity func() Entity //池中实体的生成函数
+    container chan Entity   //实体容器，以channel为载体
+}
+
+//惯例New函数，创建实体池
+func NewPool(
+    total uint32,
+    entityType reflect.Type,
+    genEntity func() Entity) (PoolIntfs, error) {
+
+    //参数校验
+    if total == 0 {
+        errMsg := fmt.Sprintf("NewPool failed.(total=%d)\n", total)
+        return nil, errors.New(errMsg)
+    }
+
+    //初始化容器载体channel
+    size := int(total)
+    container := make(chan Entity, size)
+    for i:=0; i<size; i++ {
+        newEntity := genEntity()
+        if entityType != reflect.TypeOf(newEntity) {
+            errMsg := fmt.Sprintf("New Pool failed. genEntity() is not %s\n", entityType)
+            return nil, errors.New(errMsg)
+        }
+        container <- newEntity
+    }
+
+    pool := &Pool{
+        total: total,
+        etype: entityType,
+        genEntity: genEntity(),
+        container: container,
+    }
+
+    return pool, nil
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
