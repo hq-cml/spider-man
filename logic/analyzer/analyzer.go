851e639b6010e63d039d11ec68fe35afa7fffc11
hq <hq_cml@163.com>
2017-02-19 17:48:47 +0800

    分析器结构调整

--- logic/analyzer/analyzer.go
+++ logic/analyzer/analyzer.go
@@ -18,3 +18,14 @@ type AnalyzerIntfs interface {
     Id() uint32 // 获得分析器自身Id
     Analyze(respParsers []ParseResponse, resp basic.Response) ([]basic.DataIntfs, []error) //根据规则分析响应并返回请求和条目
 }
+
+
+
+
+//分析器池类型接口
+type AnalyzerPoolIntfs interface {
+    Get() (AnalyzerIntfs, error)      // 从池中获取一个分析器
+    Put(analyzer AnalyzerIntfs) error // 归还一个分析器到池子中
+    Total() uint32                    //获得池子总容量
+    Used() uint32                     //获得正在被使用的分析器数量
+}
