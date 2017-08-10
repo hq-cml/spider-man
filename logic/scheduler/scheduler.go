ad1aea93ba3166b8c9d4bf0ff1b3883199e18a2e
hq <hq_cml@163.com>
2017-08-10 08:24:10 +0800

    Start

--- logic/scheduler/scheduler.go
+++ logic/scheduler/scheduler.go
@@ -10,6 +10,10 @@ import (
     "sync/atomic"
     "github.com/hq-cml/spider-go/middleware/channelmanager"
     "github.com/hq-cml/spider-go/logic/downloader"
+    "github.com/hq-cml/spider-go/middleware/stopsign"
+    "time"
+    "github.com/hq-cml/spider-go/helper/util"
+    "github.com/hq-cml/spider-go/basic"
 )
 
 
@@ -52,23 +56,65 @@ func (schdl *Scheduler)Start(channelLen uint, poolSize uint32, grabDepth uint32,
 
     //middleware生成
     schdl.channelManager = channelmanager.NewChannelManager(channelLen)
+
     if httpClientGenerator == nil {
         err = errors.New("The HTTP client generator list is invalid!\n") //已经开启，则退出，单例
         return
     }
-    
-    schdl.downloaderPool, err = downloader.NewDownloaderPool(poolSize,
+
+    if schdl.downloaderPool, err = downloader.NewDownloaderPool(poolSize,
         func() downloader.DownloaderIntfs{
             return downloader.NewDownloader(httpClientGenerator())
         },
-    )
-    if err != nil {
+    ); err != nil{
         err = errors.New(fmt.Sprintf("Occur error when gen downloader pool: %s\n", err))
         return
     }
 
+    if schdl.analyzerPool, err = analyzer.NewAnalyzerPool(poolSize,
+        func() analyzer.AnalyzerIntfs{
+            return analyzer.NewAnalyzer()
+        },
+    ); err != nil {
+        err = errors.New(fmt.Sprintf("Occur error when gen downloader pool: %s\n", err))
+        return
+    }
 
-    return
+    if itemProcessors == nil {
+        return errors.New("The item processor list is invalid!")
+    }
+    for i, ip := range itemProcessors {
+        if ip == nil {
+            return errors.New(fmt.Sprintf("The %dth item processor is invalid!", i))
+        }
+    }
+    schdl.processChain = processchain.NewProcessChain(itemProcessors)
+
+    if schdl.stopSign == nil {
+        schdl.stopSign = stopsign.NewStopSign()
+    } else {
+        schdl.stopSign.Reset()
+    }
+
+    schdl.reqCache = newRequestChache()
+    schdl.urlMap = make(map[string]bool)
+
+    schdl.startDownloading()
+    schdl.activateAnalyzers(respParsers)
+    schdl.openItemPipeline()
+    schdl.schedule(10 * time.Millisecond)
+
+    if firstHttpReq == nil {
+        return errors.New("The first HTTP request is invalid!")
+    }
+    if schdl.primaryDomain, err = util.GetPrimaryDomain(firstHttpReq.Host); err != nil {
+        return err
+    }
+
+    firstReq := basic.NewRequest(firstHttpReq, 0) //深度0
+    schdl.reqCache.put(firstReq)
+
+    return nil
 
 }
 
