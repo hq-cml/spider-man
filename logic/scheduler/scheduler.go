aceb0ce5b2c6356b76d155ede287a9885f5d1169
hq <hq_cml@163.com>
2017-10-05 15:51:52 +0800

    scheduler初始化重构

--- logic/scheduler/scheduler.go
+++ logic/scheduler/scheduler.go
@@ -1,5 +1,9 @@
 package scheduler
 
+/*
+ * 调度器
+ * 框架的核心，将所有的中间件和逻辑组件进行整合、同步、协调，组装成爬虫的核心逻辑
+ */
 import (
 	"errors"
 	"fmt"
@@ -23,17 +27,52 @@ func NewScheduler() *Scheduler {
 	return &Scheduler{}
 }
 
-// 开启调度器。
-// 调用该方法会使调度器创建和初始化各个组件。在此之后，调度器会激活爬取流程的执行。
-// 参数channelParams代表通道参数的容器。
-// 参数poolParams代表池基本参数的容器。
-// 参数crawlDepth代表了需要被爬取的网页的最大深度值。深度大于此值的网页会被忽略。
-// 参数httpClientGenerator代表的是被用来生成HTTP客户端的函数。
-// 参数respParsers的值应为分析器所需的被用来解析HTTP响应的函数的序列。
-// 参数entryProcessors的值应为需要被置入条目处理管道中的条目处理器的序列。
-// 参数firstHttpReq即代表首次请求。调度器会以此为起始点开始执行爬取流程。
-//TODO 重构
-func (schdl *Scheduler) Start(
+//统一Start的参数校验，对于入参进行逐个的校验
+func (schdl *Scheduler) startParamCheck(
+	context basic.Context,
+	httpClient *http.Client,
+	respAnalyzers []basic.AnalyzeResponseFunc,
+	entryProcessors []basic.ProcessEntryFunc,
+	firstHttpReq *http.Request) (error) {
+
+	if context.Conf.GrabDepth <= 0 {
+		return errors.New("GrabDepth can not be 0!")
+	}
+
+	if context.Conf.RequestChanCapcity <= 0 ||
+		context.Conf.ResponseChanCapcity <= 0 ||
+		context.Conf.EntryChanCapcity <= 0 ||
+		context.Conf.ErrorChanCapcity <= 0 {
+		return errors.New("Channel length can not be 0!")
+	}
+
+	if httpClient == nil {
+		return errors.New("The httpClient can not be nil!")
+	}
+
+	if context.Conf.DownloaderPoolSize <= 0 ||
+		context.Conf.AnalyzerPoolSize <= 0 {
+		return errors.New("Pool size can not be 0!")
+	}
+
+	if entryProcessors == nil {
+		return errors.New("The entry processor list is invalid!")
+	}
+	for i, ip := range entryProcessors {
+		if ip == nil {
+			return errors.New(fmt.Sprintf("The %dth entry processor is invalid!", i))
+		}
+	}
+
+	if firstHttpReq == nil {
+		return errors.New("The first HTTP request is invalid!")
+	}
+
+	return nil
+}
+
+//scheduler初始化
+func (schdl *Scheduler) schedulerInit(
 	context basic.Context,
 	httpClient *http.Client,
 	respAnalyzers []basic.AnalyzeResponseFunc,
@@ -57,23 +96,17 @@ func (schdl *Scheduler) Start(
 	}
 	atomic.StoreUint32(&schdl.running, RUNNING_STATUS_RUNNING)
 
-	//TODO 参数校验 & 赋值
+	//GrabDepth赋值
 	schdl.grabDepth = context.Conf.GrabDepth
 
-	//middleware生成
+	//middleware生成；通道管理器
 	schdl.channelManager = channelmanager.NewChannelManager()
-	//TODO 参数校验 &配置参数
 	schdl.channelManager.RegisterOneChannel("request", basic.NewRequestChannel(context.Conf.RequestChanCapcity))
 	schdl.channelManager.RegisterOneChannel("response", basic.NewResponseChannel(context.Conf.ResponseChanCapcity))
 	schdl.channelManager.RegisterOneChannel("entry", basic.NewEntryChannel(context.Conf.EntryChanCapcity))
 	schdl.channelManager.RegisterOneChannel("error", basic.NewErrorChannel(context.Conf.ErrorChanCapcity))
 
-	if httpClient == nil {
-		err = errors.New("The HTTP client generator list is invalid!\n") //已经开启，则退出，单例
-		return
-	}
-
-	//TODO 参数校验 & 赋值
+	//middleware生成；池管理器
 	schdl.poolManager = pool.NewPoolManager()
 	if dp, err := downloader.NewDownloaderPool(context.Conf.DownloaderPoolSize,
 		func() pool.EntityIntfs {
@@ -85,7 +118,6 @@ func (schdl *Scheduler) Start(
 	} else {
 		schdl.poolManager.RegisterOnePool("downloader", dp)
 	}
-
 	if ap, err := analyzer.NewAnalyzerPool(context.Conf.AnalyzerPoolSize, analyzer.NewAnalyzer); err != nil {
 		err = errors.New(fmt.Sprintf("Occur error when gen downloader pool: %s\n", err))
 		return err
@@ -93,42 +125,74 @@ func (schdl *Scheduler) Start(
 		schdl.poolManager.RegisterOnePool("analyzer", ap)
 	}
 
-	if entryProcessors == nil {
-		return errors.New("The entry processor list is invalid!")
-	}
-	for i, ip := range entryProcessors {
-		if ip == nil {
-			return errors.New(fmt.Sprintf("The %dth entry processor is invalid!", i))
-		}
-	}
-	schdl.processChain = processchain.NewProcessChain(entryProcessors)
-
+	//middleware生成；stopSign
 	if schdl.stopSign == nil {
 		schdl.stopSign = stopsign.NewStopSign()
 	} else {
 		schdl.stopSign.Reset()
 	}
 
+	//middleware生成；requestCache
 	schdl.requestCache = requestcache.NewRequestCache()
+
+	//processChain生成
+	schdl.processChain = processchain.NewProcessChain(entryProcessors)
+
+	//初始化已请求的URL的字典
 	schdl.urlMap = make(map[string]bool)
+
+	//主域名初始化
+	if schdl.primaryDomain, err = util.GetPrimaryDomain(firstHttpReq.Host); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+/*
+ *开启调度器。调用该方法会使调度器创建和初始化各个组件。在此之后，调度器会激活爬取流程的执行。
+ * 参数httpClient是客户端句柄。
+ * 参数respAnalyzers是用户定制的分析器链
+ * 参数entryProcessors是用户定制的处理器链
+ * 参数firstHttpReq即代表首次请求。调度器会以此为起始点开始执行爬取流程。
+ */
+func (schdl *Scheduler) Start(
+	context basic.Context,
+	httpClient *http.Client,
+	respAnalyzers []basic.AnalyzeResponseFunc,
+	entryProcessors []basic.ProcessEntryFunc,
+	firstHttpReq *http.Request) (err error) {
+
+	//错误兜底
+	defer func() {
+		if e := recover(); e != nil {
+			msg := fmt.Sprintf("Fatal Scheduler Error:%s\n", e)
+			log.Warn(msg)
+			err = errors.New(msg)
+			return
+		}
+	}()
+
+	//统一的参数校验
+	if err := schdl.startParamCheck(context, httpClient, respAnalyzers, entryProcessors, firstHttpReq); err != nil {
+		return err
+	}
+
+	//初始化sheduler
+	if err := schdl.schedulerInit(context, httpClient, respAnalyzers, entryProcessors, firstHttpReq); err != nil {
+		return err
+	}
 
 	schdl.activateDownloaders()
 	schdl.activateAnalyzers(respAnalyzers)
 	schdl.activateProcessChain()
 	schdl.schedule(10 * time.Millisecond)
 
-	if firstHttpReq == nil {
-		return errors.New("The first HTTP request is invalid!")
-	}
-	if schdl.primaryDomain, err = util.GetPrimaryDomain(firstHttpReq.Host); err != nil {
-		return err
-	}
-
+	//生成第一个请求，放入请求缓冲，调度器会自动进行后续的调度。。。
 	firstReq := basic.NewRequest(firstHttpReq, 0) //深度0
 	schdl.requestCache.Put(firstReq)
 
 	return nil
-
 }
 
 //实现Stop方法，调用该方法会停止调度器的运行。所有处理模块执行的流程都会被中止
@@ -152,8 +216,8 @@ func (schdl *Scheduler) Running() bool {
 
 //实现ErrorChan方法
 //若该方法的结果值为nil，则说明错误通道不可用或调度器已被停止。
-// 获得错误通道。调度器以及各个处理模块运行过程中出现的所有错误都会被发送到该通道。
-// 若该方法的结果值为nil，则说明错误通道不可用或调度器已被停止。
+//获得错误通道。调度器以及各个处理模块运行过程中出现的所有错误都会被发送到该通道。
+//若该方法的结果值为nil，则说明错误通道不可用或调度器已被停止。
 func (schdl *Scheduler) ErrorChan() basic.SpiderChannelIntfs {
 	//TODO 曾经出过panic 地址为空的段错误
 	if schdl.channelManager.Status() != channelmanager.CHANNEL_MANAGER_STATUS_INITIALIZED ||
